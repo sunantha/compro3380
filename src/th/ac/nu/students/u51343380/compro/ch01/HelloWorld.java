c0389d489465b0c77817ad2eb91dbc92ff77bfbc
sunantha <bow.sunantha@hotmail.com>
2012-06-25 11:02:07 +0700

    "Programming is fun"

--- src/th/ac/nu/students/u51343380/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u51343380/compro/ch01/HelloWorld.java
@@ -4,5 +4,6 @@ package th.ac.nu.students.u51343380.compro.ch01;
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello,World");
+        System.out.println("Programming is fun");
     }
 }
